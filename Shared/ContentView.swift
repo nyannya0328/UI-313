bad487d046910617621a6d2691ae522647091d8f
nyannya0328 <thecuba54@gmail.com>
2021-11-23 20:26:11 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by nyannyan0328 on 2021/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       onBoading()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/UI_313App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_313App.swift
+//  Shared
+//
+//  Created by nyannyan0328 on 2021/09/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_313App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
